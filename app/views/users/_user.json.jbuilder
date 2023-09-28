8596be9141b46b0b1d16ff41f538b007c9ee74c5
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-09-28 15:03:48 -0400

    make user model and controller and sessions controller with authentication/auth

---
+++ app/views/users/_user.json.jbuilder
@@ -0,0 +1,4 @@
+# json.extract! user, :id, :created_at, :updated_at
+# json.url user_url(user, format: :json)
+json.name user.name
+json.image_url user.image_url
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+john = User.create!(name: "John Sense", email: "john@sense.com", password: "password", image_url: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f7/Jason_Voorhees_%28Ken_Kirzinger%29.jpg/250px-Jason_Voorhees_%28Ken_Kirzinger%29.jpg")
+
+tim = User.create!(name: "Tim Curry", email: "tim@curry.com", password: "password", image_url: "https://static1.colliderimages.com/wordpress/wp-content/uploads/2020/11/tim-curry-it-pennywise-social.jpg")

5734a939223f49ee263a3b486baee3ec660cabaa
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-10-04 15:02:52 -0400

    change movies controller actions only if currentuser

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,29 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
-    render :index
+    if current_user
+      @movies = Movie.all
+      render :index
+    else
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
   end
 
   def show
-    @movie = Movie.find_by(id: params[:id])
-    render :show
+    if current_user
+      @movie = Movie.find_by(id: params[:id])
+      render :show
+    else
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
   end
 
   def most_liked
-    @most_liked_movies = Movie.joins(:favorites)
-      .group("movies.id")
-      .order("COUNT(favorites.movie_id) DESC")
-      .limit(5)
-    render json: @most_liked_movies
+    if current_user
+      @most_liked_movies = Movie.joins(:favorites)
+        .group("movies.id")
+        .order("COUNT(favorites.movie_id) DESC")
+        .limit(5)
+      render json: @most_liked_movies
+    end
   end
 end
